5e8318146b4b2e8b5332f7e4cdf85f948ad0bcaa
shashank <shashanks@gatech.edu>
2020-05-23 19:21:35 +0530

    Password Checker created

---
+++ CheckPassword.py
@@ -0,0 +1,37 @@
+import requests
+import sys
+import hashlib
+
+
+#Gives the response from the API
+def request_api_data(query_char):
+    url = "https://api.pwnedpasswords.com/range/"+ query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Fetching error....{res.status_code}. Please check the API and try again')
+    return res
+
+def password_counts(response, check):
+    web_data = (line.split(':') for line in response.text.splitlines())
+    for h, count in web_data:
+        if h == check:
+            return int(count)
+    return 0
+
+
+def check_pwned_api(password):
+    sha1password = hashlib.sha1(password.encode()).hexdigest().upper()
+    first_5, remaining = sha1password[:5], sha1password[5:]
+    response = request_api_data(first_5)
+    return password_counts(response, remaining)
+
+def main(passwords):
+    for password in passwords:
+        count = check_pwned_api(password)
+        if (count > 0):
+            print(f'password: {password} has been pwned {count} times... you should consider using a stronger one.')
+        else:
+            print(f'{password} was NOT found... WAY TO GO!')
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
