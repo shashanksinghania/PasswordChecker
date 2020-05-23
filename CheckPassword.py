dea526e5d00e33a36682600285cfeca714bbc5cd
shashank <shashanks@gatech.edu>
2020-05-23 19:31:41 +0530

    debugged

--- CheckPassword.py
+++ CheckPassword.py
@@ -11,6 +11,7 @@ def request_api_data(query_char):
         raise RuntimeError(f'Fetching error....{res.status_code}. Please check the API and try again')
     return res
 
+#Returns the count from the API
 def password_counts(response, check):
     web_data = (line.split(':') for line in response.text.splitlines())
     for h, count in web_data:
@@ -18,13 +19,14 @@ def password_counts(response, check):
             return int(count)
     return 0
 
-
+#converts the password to sha1 and returns the counts from password_counts
 def check_pwned_api(password):
     sha1password = hashlib.sha1(password.encode()).hexdigest().upper()
     first_5, remaining = sha1password[:5], sha1password[5:]
     response = request_api_data(first_5)
     return password_counts(response, remaining)
 
+#outputs the counts of passwords and suggests whether to change it or not
 def main(passwords):
     for password in passwords:
         count = check_pwned_api(password)
